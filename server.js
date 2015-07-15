ef65a73a2ad465759250d18696b8d67f760f4c16
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2015-07-15 21:32:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello on : ' + req.headers.host);
+  res.end('Hello on : ' + req.headers.host + req.method, req.url, req.path);
 }).listen(process.env.PORT);
